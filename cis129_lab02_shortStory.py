2d6fb8eff1050ea54795b62dc8d5678eabc65cfd
KeithTheTheif <kkincaid2@mail.pima.edu>
2025-02-05 11:06:13 -0700

    Update cis129_lab02_shortStory.py
    deleted hello world and added a short story

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,11 @@
-print("hello world!")
+print("On a dark and stormy night there was a freakishly huge dog. 
+every night the dog would roam around only at night near subraban homes. 
+he look for people that were not in bed specificly loud people who stayed up past their bed time. 
+suddenly the Freakisly huge dug heard a noise coming from up the street and begain to run faster than a cheeta and eventually reach the last house on the street. 
+With the dogs long skinny legs that could bend a inifinte amont times open the front door to quitly creep up on a childs room who was up at 3am play his favorite game fortnite, the boy was yelling and jumping up and down as he played. 
+The long arm ever so quitly open the door and with the snap of a finger grabbed the child and with the dog pulled back as fast as he could closing all doors with a slam behind him. 
+The boy was never seen again nor was the dog with the freakishly huge body".  ")
+
+# Author: keith kincaid
+# My first Python lab in my CIS129 course
+# Prints a simple short story about a huge dog that snatches loud people past their bed time
